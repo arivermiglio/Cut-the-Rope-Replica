bac6d4007a0142769e2cf17e68f46a7f19ef141e
arivermiglio <aria.vermiglio@mail.utoronto.ca>
2022-04-11 13:56:09 -0400

    20:18

--- Assets/Rope.cs
+++ Assets/Rope.cs
@@ -15,13 +15,17 @@ public class Rope : MonoBehaviour
     
     void GenerateRope()
     {
+        // first link connects to the hook
         Rigidbody2D previousRB = hook;
+
+        // generate links
         for (int i = 0; i < links; i++)
         {
             GameObject link = Instantiate(linkPrefab, transform);
             HingeJoint2D joint = link.GetComponent<HingeJoint2D>();
-            joint.connectedBody = hook;
+            joint.connectedBody = previousRB;
 
+            // next links should connect to current link
             previousRB = link.GetComponent<Rigidbody2D>();
         }
     }
